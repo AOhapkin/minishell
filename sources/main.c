8d0336f23af49ec4f89b3df2a831ee7ba7611c3c
AOhapkin <alexeyyago@gmail.com>
2022-05-29 21:43:50 +0300

    добавил сохранение истории работы

--- sources/main.c
+++ sources/main.c
@@ -6,6 +6,7 @@ int main(int argc, char **argv, char **envp)
 	char *line;
 
 	while_true();
+	rl_clear_history();
 	return 0;
 }
 
--- sources/while_true.c
+++ sources/while_true.c
@@ -1,5 +1,15 @@
 #include "minishell.h"
 
+char *get_input(void)
+{
+	char	*input;
+
+	input = readline(PROMPT);
+	if (isatty(STDIN_FILENO) && input)
+		add_history(input);
+	return (input);
+}
+
 void while_true(void)
 {
 	char *buffer;
@@ -8,7 +18,7 @@ void while_true(void)
 	while (TRUE)
 	{
 		// TODO добавить обертку для readline чтобы добавлялось в историю с валидацией
-		buffer = readline(PROMPT);
+		buffer = get_input();
 
 		status = lexer(buffer);
 
